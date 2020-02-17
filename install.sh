805682dc687110281dacd690e31ef91bae4f592a
Dmitry Nevolin <nevolin.dmitry.leonid@gmail.com>
2020-02-18 00:17:39 +0300

    xmonad multiple screen support + chmod 744 ~/.xinitrc

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd tilde && cp -R . ~/ && cd ..
-cd slash && sudo cp -R . /
+cd slash && sudo cp -R . / && cd ..
+chmod 744 ~/.xinitrc
--- tilde/.xmonad/xmonad.hs
+++ tilde/.xmonad/xmonad.hs
@@ -1,14 +1,22 @@
 import XMonad
+import XMonad.Actions.NoBorders(toggleBorder)
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.IndependentScreens(countScreens, withScreens, onCurrentScreen)
 import XMonad.Layout.NoBorders(smartBorders)
-import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run(spawnPipe)
 import System.IO
+
+import qualified XMonad.StackSet as W
+
+mModMask = mod4Mask
+mWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 main = do
   xmproc <- spawnPipe "xmobar"
+  mScreens <- countScreens
   xmonad $ ewmh def
     { manageHook = manageDocks <+> manageHook def
     , layoutHook = avoidStruts $ smartBorders $ layoutHook def
@@ -16,16 +24,25 @@ main = do
     , logHook = dynamicLogWithPP xmobarPP
       { ppOutput = hPutStrLn xmproc
       , ppCurrent = xmobarColor "grey" "#1B5E20" . wrap " " " "
+      , ppVisible = xmobarColor "grey" "#0d47a1" . wrap " " " "
       , ppTitle = (\str -> "")
       , ppLayout = (\str -> "")
       }
-    , modMask = mod4Mask
+    , modMask = mModMask
     , terminal = "xterm"
-    , normalBorderColor = "#5C6BC0"
+    , normalBorderColor = "#0d47a1"
     , focusedBorderColor = "#1B5E20"
     , borderWidth = 1
+    , workspaces = if mScreens > 1 then withScreens mScreens mWorkspaces else mWorkspaces
     } `additionalKeys`
-    [ ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Ubuntu Mono-14' -nb 'black' -nf 'grey' -sb '#1B5E20' -sf 'grey' -h 24")
-    , ((mod4Mask .|. shiftMask, xK_f), sendMessage ToggleStruts)
+    [ ((mModMask, xK_p), spawn "dmenu_run -fn 'Ubuntu Mono-14' -nb 'black' -nf 'grey' -sb '#1B5E20' -sf 'grey' -h 24")
+    , ((mModMask, xK_f), sendMessage ToggleStruts)
+    , ((mModMask, xK_b), withFocused toggleBorder)
     , ((0, xK_Print), spawn "scrot -e 'mv $f ~/.scrot/'")
-    ]
+    ] `additionalKeys`
+    if mScreens > 1 then
+      [ ((m .|. mModMask, k), windows $ onCurrentScreen f i)
+           | (i, k) <- zip (mWorkspaces) [xK_1 .. xK_9]
+           , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+      ]
+    else []
